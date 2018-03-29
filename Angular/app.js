af3c16d9011258b29f7157f885092bc127ec2d90
Max Gavrilov <gavrilovworks@gmail.com>
2018-03-29 12:44:26 +0300

    По клику на Edit появляется форма. Изменить - закрывает форму

--- Angular/app.js
+++ Angular/app.js
@@ -5,30 +5,30 @@ var Customers = [
         customer: 'Иванова Ирина',
         manager: 'Петрова Полина',
         status: 'Выполнен',
-        summ: 1500
+        summ: 1500,
       }, {
         number: '2',
         customer: 'Сидорова Алла',
         manager: 'Петрова Полина',
         status: 'В обработке',
-        summ: 11000
+        summ: 11000,
       }, {
         number: '3',
         customer: 'Лукьянова Прасковья',
         manager: 'Смирнов Андрей',
         status: 'Отказ',
-        summ: 15020
+        summ: 15020,
       }, {
         number: '4',
         customer: 'Гришин Сидор',
         manager: 'Смирнов Андрей',
         status: 'Выполнен',
-        summ: 4500
+        summ: 4500,
        }];
+var show = false;
 angular.module('sortApp', [])
   .controller('mainController', function () {
     this.orders = Customers;
-
-
-
-  })
+    this.showForm = show;
+  }
+  )
