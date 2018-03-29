7c22040da4521e7c9e0c6ebac63c2a428d52fe72
Max Gavrilov <gavrilovworks@gmail.com>
2018-03-29 11:38:54 +0300

    Переместил список заказов в массив Customers. Поправил сортировку.

--- Angular/app.js
+++ Angular/app.js
@@ -1,34 +1,34 @@
+    // таблица
+var Customers = [
+      {
+        number: '1',
+        customer: 'Иванова Ирина',
+        manager: 'Петрова Полина',
+        status: 'Выполнен',
+        summ: 1500
+      }, {
+        number: '2',
+        customer: 'Сидорова Алла',
+        manager: 'Петрова Полина',
+        status: 'В обработке',
+        summ: 11000
+      }, {
+        number: '3',
+        customer: 'Лукьянова Прасковья',
+        manager: 'Смирнов Андрей',
+        status: 'Отказ',
+        summ: 15020
+      }, {
+        number: '4',
+        customer: 'Гришин Сидор',
+        manager: 'Смирнов Андрей',
+        status: 'Выполнен',
+        summ: 4500
+       }];
 angular.module('sortApp', [])
-.controller ('mainController', function($scope) {
-  $scope.searchNumber   = '';
-  // таблица
-  $scope.orders = [{
-    number: '1',
-    customer: 'Иванова Ирина',
-    manager: 'Петрова Полина',
-    status: 'Выполнен',
-    summ: '1500'
-  },{
-    number: '2',
-    customer: 'Сидорова Алла',
-    manager: 'Петрова Полина',
-    status: 'В обработке',
-    summ: '11000'
-  },{
-    number: '3',
-    customer: 'Лукьянова Прасковья',
-    manager: 'Смирнов Андрей',
-    status: 'Отказ',
-    summ: '15020'
-  },{
-    number: '4',
-    customer: 'Гришин Сидор',
-    manager: 'Смирнов Андрей',
-    status: 'Выполнен',
-    summ: '4500'
-  }]
+  .controller('mainController', function () {
+    this.orders = Customers;
 
 
 
-
-})
+  })
