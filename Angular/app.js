55e51a1aa25be04faa6c8e2807b529161db4ece3
Max Gavrilov <gavrilovworks@gmail.com>
2018-03-30 15:46:47 +0300

    Добавил появление данных из таблицы в форме при клике по Edit

--- Angular/app.js
+++ Angular/app.js
@@ -1,5 +1,5 @@
     // таблица
-var Customers = [
+    var Customers = [
       {
         number: '1',
         customer: 'Иванова Ирина',
@@ -25,10 +25,34 @@ var Customers = [
         status: 'Выполнен',
         summ: 4500,
        }];
-var show = false;
-angular.module('sortApp', [])
-  .controller('mainController', function () {
-    this.orders = Customers;
-    this.showForm = show;
-  }
-  )
+
+    let show = false;
+
+    var app = angular.module('sortApp', [])
+    app.controller('mainController', function () {
+      this.orders = Customers;
+      this.showForm = show;
+      this.editForm = function (index){
+        this.showForm = true;
+        console.log(index);
+        let formNumber = document.getElementById('test-form-Number');
+        let formCustomer = document.getElementById('test-form-Customer');
+        let formManager = document.getElementById('test-form-Manager');
+        let formStatus = document.getElementById('test-form-Status');
+        let formPrice = document.getElementById('test-form-Price');
+        let numValue = this.orders[index].number;
+        let custValue = this.orders[index].customer;
+        let manValue = this.orders[index].manager;
+        let statValue = this.orders[index].status;
+        let summValue = this.orders[index].summ;
+        console.log(custValue);
+        formNumber.value = numValue;
+        formCustomer.value = custValue;
+        formManager.value = manValue;
+        formStatus.value = statValue;
+        formPrice.value = summValue;
+
+      };
+
+
+    });
