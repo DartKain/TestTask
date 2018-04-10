3faa1c2b8205f41e54d0f2f9bc21f73a0f401c19
Max Gavrilov <gavrilovworks@gmail.com>
2018-04-10 10:46:02 +0300

    Теперь ввод из формы работает

--- Angular/app.js
+++ Angular/app.js
@@ -1,58 +1,25 @@
-    // таблица
-    var Customers = [
-      {
-        number: '1',
-        customer: 'Иванова Ирина',
-        manager: 'Петрова Полина',
-        status: 'Выполнен',
-        summ: 1500,
-      }, {
-        number: '2',
-        customer: 'Сидорова Алла',
-        manager: 'Петрова Полина',
-        status: 'В обработке',
-        summ: 11000,
-      }, {
-        number: '3',
-        customer: 'Лукьянова Прасковья',
-        manager: 'Смирнов Андрей',
-        status: 'Отказ',
-        summ: 15020,
-      }, {
-        number: '4',
-        customer: 'Гришин Сидор',
-        manager: 'Смирнов Андрей',
-        status: 'Выполнен',
-        summ: 4500,
-       }];
-
-    let show = false;
+    let data = [{
+      number: 1,
+      customer: "папрла"
+    }];
 
     var app = angular.module('sortApp', [])
-    app.controller('mainController', function () {
-      this.orders = Customers;
-      this.showForm = show;
-      this.editForm = function (index){
-        this.showForm = true;
-        console.log(index);
-        let formNumber = document.getElementById('test-form-Number');
-        let formCustomer = document.getElementById('test-form-Customer');
-        let formManager = document.getElementById('test-form-Manager');
-        let formStatus = document.getElementById('test-form-Status');
-        let formPrice = document.getElementById('test-form-Price');
-        let numValue = this.orders[index].number;
-        let custValue = this.orders[index].customer;
-        let manValue = this.orders[index].manager;
-        let statValue = this.orders[index].status;
-        let summValue = this.orders[index].summ;
-        console.log(custValue);
-        formNumber.value = numValue;
-        formCustomer.value = custValue;
-        formManager.value = manValue;
-        formStatus.value = statValue;
-        formPrice.value = summValue;
+    app.controller('sortController', ['$http', function ($http) {
+      var sort = this;
 
+      sort.orders = data;
+      sort.order = undefined;
+      sort.openOrder = function (order){
+        sort.order = order;
+        console.log("123");
       };
+ sort.cancel = function (){
+   sort.order = undefined;
+ };
+      sort.add = function(){
+        let order = {};
+        sort.orders.push(order);
+        sort.order = order;
+      };
+    }]);
 
-
-    });
