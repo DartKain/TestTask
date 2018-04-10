7dd6eb36a74eeb6680df5685ef50b98043795cf3
Max Gavrilov <gavrilovworks@gmail.com>
2018-04-10 10:52:17 +0300

    Добавил массив с данными

--- Angular/app.js
+++ Angular/app.js
@@ -1,6 +1,27 @@
     let data = [{
       number: 1,
-      customer: "папрла"
+        customer: 'Иванова Ирина',
+        manager: 'Петрова Полина',
+        status: 'Выполнен',
+        summ: 1500
+      }, {
+        number: 2,
+        customer: 'Сидорова Алла',
+        manager: 'Петрова Полина',
+        status: 'В обработке',
+        summ: 11000
+      }, {
+        number: 3,
+        customer: 'Лукьянова Прасковья',
+        manager: 'Смирнов Андрей',
+        status: 'Отказ',
+        summ: 15020
+      }, {
+        number: 4,
+        customer: 'Гришин Сидор',
+        manager: 'Смирнов Андрей',
+        status: 'Выполнен',
+        summ: 4500
     }];
 
     var app = angular.module('sortApp', [])
