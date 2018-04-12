b8ebfb6880cafbd1723a33e97fdb9fe87e72622b
Max Gavrilov <gavrilovworks@gmail.com>
2018-04-12 14:22:31 +0300

    Добавил выпадающие списки для сортировки

--- Angular/app.js
+++ Angular/app.js
@@ -1,28 +1,39 @@
     let data = [{
       number: 1,
-        customer: 'Иванова Ирина',
-        manager: 'Петрова Полина',
-        status: 'Выполнен',
-        summ: 1500
+      customer: 'Иванова Ирина',
+      manager: 'Петрова Полина',
+      status: 'Выполнен',
+      summ: 1500
       }, {
-        number: 2,
-        customer: 'Сидорова Алла',
-        manager: 'Петрова Полина',
-        status: 'В обработке',
-        summ: 11000
+      number: 2,
+      customer: 'Сидорова Алла',
+      manager: 'Петрова Полина',
+      status: 'В обработке',
+      summ: 11000
       }, {
-        number: 3,
-        customer: 'Лукьянова Прасковья',
-        manager: 'Смирнов Андрей',
-        status: 'Отказ',
-        summ: 15020
+      number: 3,
+      customer: 'Лукьянова Прасковья',
+      manager: 'Смирнов Андрей',
+      status: 'Отказ',
+      summ: 15020
       }, {
-        number: 4,
-        customer: 'Гришин Сидор',
-        manager: 'Смирнов Андрей',
-        status: 'Выполнен',
-        summ: 4500
+      number: 4,
+      customer: 'Гришин Сидор',
+      manager: 'Смирнов Андрей',
+      status: 'Выполнен',
+      summ: 4500
     }];
+
+    let type = [ {sortCol: 'number'
+                 }, {
+                   sortCol: 'customer'
+                 }, {
+                   sortCol: 'manager'
+                 }, {
+                   sortCol: 'status'
+                 }, {
+                   sortCol: 'summ'
+                 }]
 
     var app = angular.module('sortApp', [])
     app.controller('sortController', ['$http', function ($http) {
@@ -30,17 +41,35 @@
 
       sort.orders = data;
       sort.order = undefined;
-      sort.openOrder = function (order){
+
+      sort.colType = type;
+
+      sort.sortType = 'number';
+
+      sort.sortOrder = false;
+
+      sort.openOrder = function (order) {
         sort.order = order;
         console.log("123");
       };
- sort.cancel = function (){
-   sort.order = undefined;
- };
-      sort.add = function(){
+
+      sort.cancel = function () {
+        sort.order = undefined;
+      };
+
+      sort.add = function () {
         let order = {};
         sort.orders.push(order);
         sort.order = order;
       };
-    }]);
 
+      sort.remove = function (order){
+      let order = {};
+        sort.orders.splice(order);
+
+      };
+
+      sort.optionOne = function (){
+        console.log ("123321");
+      };
+    }]);
